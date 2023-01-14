7a16c0318f18b47cbf1ea50d31befd89b677fc39
trieulecit <trieu.le.cit19@eiu.edu.vn>
2023-01-14 09:33:28 +0700

    Test Hauk 1

---
+++ src/hau_trieu/chat/infrastructure/data/UserRepositoryImpl.java
@@ -0,0 +1,31 @@
+package hau_trieu.chat.infrastructure.data;
+
+import hau_trieu.chat.User;
+
+public class UserRepositoryImpl implements UserRepository {
+
+	@Override
+	public User get(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
