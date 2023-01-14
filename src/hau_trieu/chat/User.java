fc4b2f5233e865585aebb78854b04953011273d7
trieulecit <trieu.le.cit19@eiu.edu.vn>
2023-01-14 09:16:12 +0700

    initial commit

---
+++ src/hau_trieu/chat/User.java
@@ -0,0 +1,66 @@
+package hau_trieu.chat;
+
+public class User {
+	private String lastName;
+	private String firstName;
+	private String fullName;
+	private String hashPassword;
+	private String gender;
+	private String dateOfBirth;
+	
+	public User(String lastName, String firstName, String hashPassword, String gender, String dob) {
+		super();
+		this.lastName = lastName;
+		this.firstName = firstName;
+		fullName = lastName + " " + firstName;
+		this.hashPassword = hashPassword;
+		this.gender = gender;
+		this.dateOfBirth = dob;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getFullName() {
+		return fullName;
+	}
+
+	public String getHashPassword() {
+		return hashPassword;
+	}
+
+	public void setHashPassword(String hashPassword) {
+		this.hashPassword = hashPassword;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	
+}
