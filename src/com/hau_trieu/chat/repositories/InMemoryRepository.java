6bfbdef59aea5dd31a4f83fb32f44ff45c97d40a
trieulecit <trieu.le.cit19@eiu.edu.vn>
2023-02-18 09:05:24 +0700

    Repo related

---
+++ src/com/hau_trieu/chat/repositories/InMemoryRepository.java
@@ -0,0 +1,33 @@
+package com.hau_trieu.chat.repositories;
+
+import java.util.function.Predicate;
+
+import com.hau_trieu.chat.domains.BaseEntity;
+
+public class InMemoryRepository<T extends BaseEntity> implements Repository<T> {
+
+	@Override
+	public T getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(T addingEntity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T getFirst(Predicate<T> predicate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/hau_trieu/chat/repositories/Repository.java
@@ -0,0 +1,13 @@
+package com.hau_trieu.chat.repositories;
+
+import java.util.function.Predicate;
+
+import com.hau_trieu.chat.domains.BaseEntity;
+
+public interface Repository<T extends BaseEntity> {
+	T getById(int id);
+    boolean add(T addingEntity);
+    void deleteAll();
+
+    T getFirst(Predicate<T> predicate);
+}
