de7ca4ef1e463c3930ce841b5e6394ff25ff3a16
trieulecit <trieu.le.cit19@eiu.edu.vn>
2023-03-04 09:29:09 +0700

    Update User Test for get all users matching string

--- src/test/UserServiceTest.java
+++ src/test/UserServiceTest.java
@@ -2,6 +2,10 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import com.hautrieu.chat.data.DataStorage;
 import com.hautrieu.chat.data.InMemoryDataStorage;
 import com.hautrieu.chat.domains.User;
+import com.hautrieu.chat.repositories.Repository;
 import com.hautrieu.chat.services.TextService;
 import com.hautrieu.chat.services.UserService;
 
@@ -17,36 +22,67 @@ class UserServiceTest {
 	UserService service;
 	DataStorage dataStorage;
 	TextService textService;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		dataStorage = InMemoryDataStorage.getInstance();
 		service = new UserService(dataStorage);
 		textService = new TextService();
-//		service.create(testUser);
 
+	}
+	
+	@AfterEach
+	void clearData() {
+		dataStorage.getGroups().deleteAll();
+		dataStorage.getUsers().deleteAll();
 	}
 
 	@DisplayName("Sign up test")
 	@Test
-	void test() {
-		assertTrue(service.addUser("hau", "admin"));
+	void testSignUp() {
+		assertTrue(service.addUser("trieu", "trieu"));
+		assertTrue(service.addUser("hau", "hau"));
+		assertTrue(service.addUser("somebody", "somebody"));				
 	}
 
 	@DisplayName("Log in test")
+
 	@Test
-	void test2() {
+	void testLogin() {
 		service.addUser("hau", "admin");
 		assertTrue(service.login("hau", "admin"));
 	}
 
 	@DisplayName("Hash Test")
 	@Test
-	void test3() {
-		
+	void testHashing() {
 		String nonHash = "123456";
 		String expected = "e10adc3949ba59abbe56e057f20f883e";
 		String actual = textService.hashMD5(nonHash);
 		assertEquals(expected, actual);
+
+	}
+
+	@DisplayName("Get All Users Matching a string")
+	@Test
+	void testGetAllUsersMatching() {
+		assertTrue(service.addUser("trieu", "trieu"));
+		assertTrue(service.addUser("somebody", "somebody"));
+
+		Repository<User> users = dataStorage.getUsers();
+		
+		User firstUser = users.getFirst(user -> user.getUsername().equals("somebody"));
+		firstUser.setFirstName("Trieu");
+		firstUser.setLastName("Le Hoang");
+
+		User secondUser = users.getFirst(user -> user.getUsername().equals("trieu"));
+		secondUser.setFirstName("Trieu");
+		secondUser.setLastName("Le Hoang");
+
+
+		List<User> usersMatched = service.getUsers("Trieu Le Hoang");
+
+		assertEquals(2, usersMatched.size());
 
 	}
 
